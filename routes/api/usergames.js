bb7f2c6640c1d7428feae16cd4cf14b08c1caa45
zseever <zseever@gmail.com>
2022-09-09 13:01:33 -0700

    Deploy fix

--- routes/api/usergames.js
+++ routes/api/usergames.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userGamesCtrl = require('../../controllers/api/userGames');
+const userGamesCtrl = require('../../controllers/api/usergames');
 
 router.get('/list', userGamesCtrl.list);
 router.post('/list/games/:id', userGamesCtrl.addToList);
